60727878e8fa1d1a9abad5e99ea5e7533ff62563
Anirudh Kottayil Prince <anirudhkottayil@gmail.com>
2026-04-10 00:56:33 +1000

    feat: add schema and db initialisation logic

--- db.c
+++ db.c
@@ -1,5 +1,6 @@
 #include "db.h"
 #include <stdio.h>
+#include "schema.h"
 
 int initialize_db(sqlite3 **db){
   int out = sqlite3_open_v2("study-budget.db",
@@ -11,4 +12,28 @@ int initialize_db(sqlite3 **db){
     sqlite3_close(*db);
     return 1;
   } else {return 0;}
+}
+
+int first_run(sqlite3 *db){
+  sqlite3_stmt *ppStmt = NULL;
+  const char *pZtail = NULL;
+  const char* sql = DB_SCHEMA;
+  int rc;
+  while( sql[0] != '\0') {
+    rc = sqlite3_prepare_v2(db,sql, -1,&ppStmt, &pZtail);
+    if (rc != SQLITE_OK){ return rc; }
+
+    if (ppStmt != NULL){
+      rc = sqlite3_step(ppStmt);
+      sqlite3_finalize(ppStmt);
+      if (rc != SQLITE_DONE){ return rc; }
+    }
+    if (pZtail == sql){ break; }
+    sql = pZtail;
+  }
+  return SQLITE_OK;
+}
+
+int first_write_into_db(sqlite3* db, int arr[], char** sub_arr){
+  
 }
--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "db.h"
 #include "log.h"
 
@@ -9,19 +11,8 @@ int main(){
 
   int is_first = check_first_time_run();
   if (is_first){
-    int bed_time, wakeup, sleep_quality, mood, energy;
-    printf("Enter time you went to bed: ");
-    scanf("%d\n", &bed_time);
-    printf("Enter time you woke up: ");
-    scanf("%d\n", &wakeup);
-    printf("Enter sleep quality (1 to 5): ");
-    scanf("%d\n", &sleep_quality);
-    printf("Enter mood (1 to 5): ");
-    scanf("%d\n", &mood);
-    printf("Enter energy (1 to 5): ");
-    scanf("%d\n", &energy);
-    
-    
+
+     
   }
   return 0;
 }
