b210abe18fac400e926e66892b8e717615933214
Anirudh Kottayil Prince <anirudhkottayil@gmail.com>
2026-04-15 01:32:47 +1000

    fix: wrong order of reading row from db

--- src/mapper_func.c
+++ src/mapper_func.c
@@ -11,8 +11,8 @@ void map_subject(sqlite3_stmt* ppStmt, void* arr, int index){
 void map_tasks(sqlite3_stmt* ppStmt, void* arr, int index){
   Task* tasks = (Task*) arr;
       tasks[index].id = sqlite3_column_int (ppStmt, 0);
-      tasks[index].estimated_mins = sqlite3_column_int (ppStmt, 1);
-      tasks[index].observed_mins = sqlite3_column_int (ppStmt, 2);
-      strncpy(tasks[index].task,(const char*) sqlite3_column_text(ppStmt, 3), 49);
+      strncpy(tasks[index].task,(const char*) sqlite3_column_text(ppStmt, 1), 49);
       tasks[index].task[49] = '\0';
+      tasks[index].estimated_mins = sqlite3_column_int (ppStmt, 2);
+      tasks[index].observed_mins = sqlite3_column_int (ppStmt, 3);
 }
