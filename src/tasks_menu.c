7c4866d88f340ea5dc84880bd78727ffc56c724b
Anirudh Kottayil Prince <anirudhkottayil@gmail.com>
2026-04-17 18:12:58 +1000

    feat: add update task time

--- src/tasks_menu.c
+++ src/tasks_menu.c
@@ -4,6 +4,39 @@
 #include "tasks_menu.h"
 #include "sql_commands.h"
 
+int update_task(sqlite3* db, Task** tasks, int* num_tasks, int* in_study){
+  int arr[3]; int rc; int fin_task;
+  printf("Which task did you work on\n");
+  printf("| %-3s | %-20s |\n", "ID", "TASK");
+  printf("|-----|----------------------|\n");
+  for (int i = 0; i < *num_tasks; i++){
+      printf("| %-3d | %-20s |\n",(*tasks)[i].id,(*tasks)[i].task);
+  }
+  scanf("%d",&arr[1]);
+  printf("Enter time you spent on the task right now (in mins): ");
+  scanf("%d", &arr[0]);
+  arr[0] = (int) arr[0];
+  getchar();
+
+  rc = sql_command_exec(db, "tasks", update_task_time, arr, 2, NULL, NULL);
+
+  if (rc) {
+    fprintf(stderr, "Update task time failed\n");
+    return -1;
+  } 
+
+  printf("Did you finish the task ? (1 if yes)\n");
+  scanf("%d",&fin_task);
+
+  if (fin_task){
+    arr[0] = 1;
+    rc = sql_command_exec(db, "tasks", complete_task, arr, 1, NULL, NULL);
+    if (rc) return -1;
+  }
+
+  return 0;
+}
+
 int add_task(sqlite3* db, Task** tasks, int* num_tasks, char* name){
   int add_idx = -1;
   for (int i = 0; i < *num_tasks; i++){
@@ -63,8 +96,9 @@ int tasks_menu(sqlite3* db, int* in_study, Task** tasks, int* num_tasks){
     }
     printf("Enter 1 to view tasks\n");
     printf("Enter 2 to add a task\n");
-    printf("Enter 3 to mark a task complete\n");
-    printf("Enter 4 to return to main menu\n");
+    printf("Enter 3 to update time in a task\n");
+    printf("Enter 4 to mark a task complete\n");
+    printf("Enter 5 to return to main menu\n");
     scanf("%d", &user_input);
     getchar();
 
@@ -89,13 +123,7 @@ int tasks_menu(sqlite3* db, int* in_study, Task** tasks, int* num_tasks){
         printf("Success\n");
       }
     } else if (user_input == 3){
-      char subject[50];
-      printf("Enter the name of the subject you want to add: ");
-      scanf("%49s", subject);
-      getchar();
-      subject[49] = '\0';
-      printf("\n");
-      rc = delete_subject(db, subjects, num_subjects, subject);
+        rc = update_task(db, tasks, num_tasks, in_study);
       if (rc == 1) {
         return 1;
       } else if (rc == -1) {
