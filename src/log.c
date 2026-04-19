bbcab0d881e8900104e3e733a51f3d0934b7330d
Anirudh Kottayil Prince <anirudhkottayil@gmail.com>
2026-04-19 18:56:59 +1000

    fix: memory leak in subjects array

--- src/log.c
+++ src/log.c
@@ -22,29 +22,31 @@ int check_first_time_run(){
 }
 
 int first_time_data(sqlite3* db){
-    int arr[7];
-    printf("Enter time you went to bed: ");
-    scanf("%d", &arr[0]);
-    printf("Enter time you woke up: ");
-    scanf("%d", &arr[1]);
-    printf("Enter sleep quality (1 to 5): ");
-    scanf("%d", &arr[2]);
-    printf("Enter mood (1 to 5): ");
-    scanf("%d", &arr[3]);
-    printf("Enter energy (1 to 5): ");
-    scanf("%d", &arr[4]);
-    printf("Enter your bank balance: ");
-    scanf("%d", &arr[5]);
-    printf("Enter how many subjects do you want: ");
-    scanf("%d", &arr[6]);
-    getchar();
-    char** sub_arr = malloc (arr[6]* sizeof(char*));
-    for (int i = 0; i < arr[6]; i++){
-      sub_arr[i] = malloc(50 * sizeof(char));
-      fgets( sub_arr[i], 50*sizeof(char), stdin);
-      sub_arr[i][strcspn(sub_arr[i], "\n")] = '\0';
-    }
-    first_write_into_db(db, arr, sub_arr);
+  int arr[7];
+  printf("Enter time you went to bed: ");
+  scanf("%d", &arr[0]);
+  printf("Enter time you woke up: ");
+  scanf("%d", &arr[1]);
+  printf("Enter sleep quality (1 to 5): ");
+  scanf("%d", &arr[2]);
+  printf("Enter mood (1 to 5): ");
+  scanf("%d", &arr[3]);
+  printf("Enter energy (1 to 5): ");
+  scanf("%d", &arr[4]);
+  printf("Enter your bank balance: ");
+  scanf("%d", &arr[5]);
+  printf("Enter how many subjects do you want: ");
+  scanf("%d", &arr[6]);
+  getchar();
+  char** sub_arr = malloc (arr[6]* sizeof(char*));
+  for (int i = 0; i < arr[6]; i++){
+    sub_arr[i] = malloc(50 * sizeof(char));
+    fgets( sub_arr[i], 50*sizeof(char), stdin);
+    sub_arr[i][strcspn(sub_arr[i], "\n")] = '\0';
+  }
+  first_write_into_db(db, arr, sub_arr);
+  for (int i = 0; i < arr[6]; i++) free(sub_arr[i]);
+  free(sub_arr);
   return 0;
 }
 
