b2767baf312205ca0d27dfff982a6f5e37f56e6d
Anirudh Kottayil Prince <anirudhkottayil@gmail.com>
2026-04-20 09:31:13 +1000

    fix: tried changing name to fix clangd error

---
+++ src/sql_commands.h
@@ -0,0 +1,31 @@
+#ifndef SQLCOMMANDS_H
+#define SQLCOMMANDS_H
+
+extern const char *insert_daily_log;
+extern const char *first_log;
+extern const char *first_bank_balance;
+extern const char *insert_subjects;
+extern const char *insert_study_session;
+extern const char *insert_task;
+extern const char *update_task_time;
+extern const char *complete_task;
+extern const char *get_incomplete_tasks;
+extern const char *count_subjects;
+extern const char *count_tasks;
+extern const char *get_subjects;
+extern const char *count_daily_logs;
+extern const char *get_n_logs;
+extern const char *get_log;
+extern const char *update_daily_log;
+extern const char *count_expenses;
+extern const char *get_expenses;
+extern const char *update_expense;
+extern const char *delete_subject_sql;
+extern const char *count_income_events;
+extern const char *get_income_events;
+extern const char *insert_income;
+extern const char *update_income;
+extern const char *get_last_log_date;
+extern const char *update_eod_log;
+
+#endif
